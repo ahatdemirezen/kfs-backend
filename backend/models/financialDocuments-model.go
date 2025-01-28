73f1369a069ed6e6d85faacd907de72ae83fdc45
ahatdemirezen <ahatdemirezenn@gmail.com>
2025-01-28 16:01:07 +0300

    financial Table Update

--- backend/models/financialDocuments-model.go
+++ backend/models/financialDocuments-model.go
@@ -6,10 +6,10 @@ import (
 
 // FinancialDocuments modeli
 type FinancialDocuments struct {
-	FinancialDocumentId uint      `gorm:"primaryKey;autoIncrement;column:id"`    // Primary key
-	CampaignId          uint      `gorm:"not null;column:campaign_id"`           // Foreign key
-	Subject             string    `gorm:"type:text;not null;column:subject"`     // Konu başlığı
-	DocumentKey         int       `gorm:"type:int;not null;column:document_key"` // Belge anahtarı
+	FinancialDocumentId uint      `gorm:"primaryKey;autoIncrement;column:id"`             // Primary key
+	CampaignId          uint      `gorm:"not null;column:campaign_id"`                    // Foreign key
+	Subject             string    `gorm:"type:text;not null;column:subject"`              // Konu başlığı
+	DocumentKey         string    `gorm:"type:varchar(255);not null;column:document_key"` // Belge anahtarı
 	CreatedAt           time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;column:created_at"`
 	UpdatedAt           time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;column:updated_at"`
 
