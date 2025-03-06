4dce1b7131248a2c11eec6ec2c26f53de2de7fca
Abdulkadir <kattaskadir0@gmail.com>
2025-03-06 13:14:24 +0300

    .env Path

--- backend/config/env-config.go
+++ backend/config/env-config.go
@@ -22,7 +22,7 @@ var AppConfig *Config
 
 func LoadConfig() {
 	// .env dosyasını yükle
-	err := godotenv.Load()
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
